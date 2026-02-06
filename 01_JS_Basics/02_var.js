a8c9c075a7df65df7f51d046b028ec5ff5452758
wizzy-abhishek <workspace.abhishek.08@gmail.com>
2026-02-07 00:15:27 +0530

    variables

---
+++ 01_JS_Basics/02_var.js
@@ -0,0 +1,11 @@
+let age = 20
+let name = 'Abhishek Anand'
+var incosistentScope = 'I am accessible everywhere'
+
+anotherWay = 'We can define variables without var, let or const but it is not recommended. \
+This will create a global variable and can lead to unexpected behavior in larger codebases.\
+Even if you define it inside a function, it will still be accessible outside the function.'
+
+const finalVal = 200
+
+console.log(anotherWay)
