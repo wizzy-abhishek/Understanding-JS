a30ef1c81e5c45079db261d42a75f24b1a3b29a0
wizzy-abhishek <workspace.abhishek.08@gmail.com>
2026-02-08 02:02:31 +0530

    String

---
+++ 01_JS_Basics/07_string.js
@@ -0,0 +1,8 @@
+let myName = new String('Abhishek Anand')
+
+console.log(myName.length);
+console.log(myName.slice(-14, -4));
+console.log(myName.slice(-13, 4));
+console.log(myName.slice(-12, 4));
+
+console.log(myName.includes("Ana"));
