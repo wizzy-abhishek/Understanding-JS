b1bb5a79d5508f7d9e18f86e7e485b490e174ab2
wizzy-abhishek <workspace.abhishek.08@gmail.com>
2026-02-15 13:39:56 +0530

    Object

---
+++ 02_JS_Medium /04_object.js
@@ -0,0 +1,27 @@
+// There are two types of objects 
+// 1. created by contructor (Singleton)
+// 2. Object literal
+
+const mySymbol = Symbol("Key")
+
+let user = {
+    name: "Abhishek",
+    "full name": "Abhishek Anand",
+    age: 21,
+    email: "Workspace.abhishek.08@gmail.com",
+    logged_in: true,
+    [mySymbol]: "*"
+}
+
+console.log(user);
+
+// Accessing any field 
+
+console.log(user.name);
+
+console.log(user.mySymbol); // Symbols can't be accesed like this
+console.log(user[mySymbol]);
+
+console.log(user["full name"]);
+
+Object.freeze(user) // Now the user object is final
